0eadb121e3bd555bee6b6536eeb6e5591a21b7f6
gghatano <adad0405@gmail.com>
2018-11-14 04:32:40 +0000

    ディレクトリ構成だけ決めた

---
+++ utility/calc_utility.py
@@ -0,0 +1,7 @@
+
+
+
+
+
+
+## calc_item_item_sim_diff(dat_raw, dat_anon)
---
+++ utility/item_item_sim_diff.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from scipy import sparce
+
+def calc_item_item_sim_diff(dat_raw, dat_anon):
+	## item_userへの変換
+	## item_user matrix
+	## item item sim 
+  
+	return 0
